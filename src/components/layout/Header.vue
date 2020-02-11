a5e2bf60f630ee8459012b17bdd1fa61e608a537
Martin Morset <mamor@dfds.com>
2020-02-11 23:23:45 +0100

    vuex to handle user auth

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,20 +1,44 @@
 <template>
   <header class="header">
     <div id="nav">
-        <router-link to="/">Home</router-link> 
-        <router-link to="/about">About</router-link> 
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <!--<template v-if="user.loggedIn">-->
         <router-link :to="{name: 'login'}">Login</router-link>
-        <!--|<router-link to="/register">Register</router-link>-->
-        <router-link to="/">
-          <img src="../../assets/logo.png" />
-        </router-link>
+        <div v-if="user.loggedIn">{{user.data.displayName}}</div>
+      <!--</template>-->
+      <!--<template v-else>
+        <a class="nav-link" @click.prevent="signOut">Sign out</a>
+      </template>-->
+      <router-link to="/">
+        <img src="../../assets/logo.png" />
+      </router-link>
     </div>
   </header>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+import { auth } from "@/firebase";
 export default {
-  name: "Header"
+  name: "Header",
+  computed: {
+    ...mapGetters({
+      // map `this.user` to `this.$store.getters.user`
+      user: "user"
+    })
+  },
+  methods: {
+    signOut() {
+      auth
+        .signOut()
+        .then(() => {
+          this.$router.replace({
+            name: "home"
+          });
+        });
+    }
+  }
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import VueFlashMessage from "vue-flash-message";
 import { firestorePlugin } from "vuefire";
+import store from "./store";
 
 // Vue.js initialization
 Vue.use(firestorePlugin);
 Vue.use(VueFlashMessage);
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store
 }).$mount("#app");
